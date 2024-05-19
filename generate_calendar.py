daf855adba0a887dab67855087575f64add4507d
AJ Jordan <alex@strugee.net>
2024-05-19 16:59:52 -0400

    Run `black`

--- generate_calendar.py
+++ generate_calendar.py
@@ -6,13 +6,15 @@ from ical.calendar_stream import IcsCalendarStream
 from ical.event import Event
 from datetime import datetime, date
 
+
 def get_summer_solstice(year):
     observer = ephem.Observer()
-    observer.lat = '0'  # Latitude of the observer (Equator)
-    observer.lon = '0'  # Longitude of the observer (Prime Meridian)
+    observer.lat = "0"  # Latitude of the observer (Equator)
+    observer.lon = "0"  # Longitude of the observer (Prime Meridian)
 
     solstice_date = ephem.next_summer_solstice(datetime(year, 1, 1))
     return solstice_date.datetime().date()
+
 
 calendar = Calendar()
 current_year = datetime.now().year
@@ -24,7 +26,11 @@ for year in range(current_year, current_year + 20):
     day = solstice_datetime.day
     print(year, month, day)
     calendar.events.append(
-        Event(summary="Volunteer Responsibility Amnesty Day", start=date(year, month, day), end=date(year, month, day+1)),
+        Event(
+            summary="Volunteer Responsibility Amnesty Day",
+            start=date(year, month, day),
+            end=date(year, month, day + 1),
+        ),
     )
 
 print(IcsCalendarStream.calendar_to_ics(calendar))
