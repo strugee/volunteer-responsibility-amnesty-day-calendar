8617bd98e727d5254b30dffed79b7e6adb886c22
AJ Jordan <alex@strugee.net>
2024-05-19 16:56:42 -0400

    Print a calendar to stdout

--- generate_calendar.py
+++ generate_calendar.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 import ephem
-from datetime import datetime
+from ical.calendar import Calendar
+from ical.calendar_stream import IcsCalendarStream
+from ical.event import Event
+from datetime import datetime, date
 
 def get_summer_solstice(year):
     observer = ephem.Observer()
@@ -11,7 +14,17 @@ def get_summer_solstice(year):
     solstice_date = ephem.next_summer_solstice(datetime(year, 1, 1))
     return solstice_date.datetime().date()
 
+calendar = Calendar()
 current_year = datetime.now().year
-for i in range(current_year, current_year + 20):
-    print(f"Summer Solstice in {i}: {get_summer_solstice(i)}")
+for year in range(current_year, current_year + 20):
+    solstice_datetime = get_summer_solstice(year)
+    print(f"Summer Solstice in {year}: {solstice_datetime}")
 
+    month = solstice_datetime.month
+    day = solstice_datetime.day
+    print(year, month, day)
+    calendar.events.append(
+        Event(summary="Volunteer Responsibility Amnesty Day", start=date(year, month, day), end=date(year, month, day+1)),
+    )
+
+print(IcsCalendarStream.calendar_to_ics(calendar))
